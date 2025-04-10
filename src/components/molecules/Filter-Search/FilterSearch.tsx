c6c2efc4b2a8917894fb447f5a00d59d8eecfa3e
johan9846 <johan.seb9846@gmail.com>
2025-04-09 22:59:18 -0500

    [FEATURE] implement inputSearch

--- src/components/molecules/Filter-Search/FilterSearch.tsx
+++ src/components/molecules/Filter-Search/FilterSearch.tsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
-
 import lenIcon from "../../../assets/icons/len_icon.svg";
+import { filterResults } from "../../../utils/charactersUtils";
 
 const FilterSearch = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
+  const handleSearch = (e: { target: { value: string } }) => {
+    setSearchTerm(e.target.value);
+    filterResults(e.target.value);
+  };
+
   return (
-    <div>
-      <span className="flex relative items-center justify-between w-full bg-[#F3F4F6] h-52 rounded-[8px] pt-9 pr-13 pb-9 pl-20">
-        <img src={lenIcon} alt="lenIcon" />
-        <label className="flex relative w-full h-34">
-          <input
-            className="w-full h-full outline-none border-none text-custom_gray text-14 bg-transparent p-2"
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search or filter results"
-            value={searchTerm}
-          />
-        </label>
-      </span>
+    <div className="flex">
+      <img src={lenIcon} alt="Search Icon"  />
+      <label className="flex relative w-full h-full">
+        <input
+          className="w-full h-full outline-none border-none text-custom_gray text-[14px] bg-transparent px-2"
+          onChange={handleSearch}
+          placeholder="Search or filter results"
+          value={searchTerm}
+        />
+      </label>
     </div>
   );
 };
--- src/components/organisms/Filter-APP/FilterApp.tsx
+++ src/components/organisms/Filter-APP/FilterApp.tsx
@@ -1,34 +1,19 @@
-import { filterResults, orderList } from "../../../utils/charactersUtils";
-import lenIcon from "../../../assets/icons/len_icon.svg";
+import { orderList } from "../../../utils/charactersUtils";
 import ListOrder from "../../molecules/List-Order/ListOrder";
 import FilterMenu from "../../molecules/Filter-Menu/FilterMenu";
 import { useReactiveVar } from "@apollo/client";
 import { filterVar } from "../../../apollo/reactiveVars";
-
+import FilterSearch from "../../molecules/Filter-Search/FilterSearch";
 
 const Filter = () => {
   const filterState = useReactiveVar(filterVar);
-  const handleSearch = (e: any) => {
-    filterResults(e.target.value);
-  };
-
 
   return (
     <>
-      <span className="flex relative items-center justify-between w-full bg-[#F3F4F6] h-52 rounded-[8px] ">
-        <img src={lenIcon} alt="lenIcon" className="pl-20 pr-8" />
-        <label className="flex relative w-full h-34">
-          <input
-            className="w-full h-full outline-none border-none text-custom_gray text-14 bg-transparent "
-            onChange={handleSearch}
-            placeholder="Search or filter results"
-          />
-        </label>
-        <div>
-          <FilterMenu />
-        </div>
-      </span>
-
+      <div className="flex relative items-center justify-between w-full bg-[#F3F4F6] h-[52px] rounded-[8px] pt-9 pr-13 pb-9 pl-20">
+        <FilterSearch />
+        <FilterMenu />
+      </div>
 
       <div className="flex justify-between mt-16">
         <p className="text-16 text-custom_blue font-sans font-medium">
@@ -37,7 +22,9 @@ const Filter = () => {
 
         <section className="bg-secondary600 bg-opacity-20 min-w-min-64 min-h-24 rounded-[12px] pr-12 pl-12 flex items-center justify-center">
           <p className="text-custom_green text-14 font-sans font-medium">
-            {`${filterState?.selectedFiltersCount || 0} filter${filterState?.selectedFiltersCount > 1 ? "s" : ""}`}
+            {`${filterState?.selectedFiltersCount || 0} filter${
+              filterState?.selectedFiltersCount > 1 ? "s" : ""
+            }`}
           </p>
         </section>
       </div>
